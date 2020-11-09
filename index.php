3340ada9610637bfee4205bccf9d73c99adc7946
chimaikeji <chima.ikeji@ymail.com>
2020-11-09 15:50:57 +0100

    first code push

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+        <title>Eaglon</title>
+</head>
+<body>
+    <section id="banner">
+        <div class="banner1">
+            <img src="images/banner1.jpg">
+            <div class="text-box text-box1">
+                <h1>EAGLON SERVICES NIGERIA</h1>
+                <span></span>
+                <h3>BUILDERS/GENERAL CONTRACORS</h3>
+                <p>"We Shape Our Buildings; thereafter, our building shapes us." </p><br>
+                <p></p>
+            </div>
+        </div>
+        <div class="banner2">
+            <img src="images/banner2.jpg">
+            <div class="text-box text-box2">
+                <h1>EAGLON SERVICES NIGERIA</h1>
+                <span></span>
+                <h3>HOUSE HOLD CHEMICAL</h3>
+                <p>"We Shape Our Buildings; thereafter, our building shapes us." </p><br>
+                <p></p>
+            </div>
+        </div>
+        <div class="banner3">
+            <img src="images/banner3.jpg">
+            <div class="text-box text-box3">
+                <h1>EAGLON SERVICES NIGERIA</h1>
+                <span></span>
+                <h3>FASHION COLLECTION</h3>
+                <p>"We Shape Our Buildings; thereafter, our building shapes us." </p><br>
+                <p></p>
+            </div>
+        </div>
+        <div class="banner4">
+            <img src="images/banner4.jpg">
+            <div class="text-box text-box4">
+                <h1>EAGLON SERVICES NIGERIA</h1>
+                <span></span>
+                <h3>BUILDERS/GENERAL CONTRACORS</h3>
+                <p>"We Shape Our Buildings; thereafter, our building shapes us." </p><br>
+                <p></p>
+            </div>
+        </div>
+    </section>
+    <section id="nav-bar">
+        <div class="nav">
+            <a href="#"><img src="images/Logo.JPG" alt="Eaglon"></a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Porfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="about">
+        <div class="container1">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2>ABOUT US</h2>
+                    <p> Eaglon Services is a Nigerian company registered on January 22, 2010 with the Corporate Affairs Commission.
+                        Eaglon fully commenced business immediately after registration in January 2010 at Okechukwu Njoku’s
+                        Compound in Umuakali, Naze, Owerri North Local Government Area in Imo State, Nigeria. Eaglon specializes
+                        in General Contracts, Block Making, Cosmetic Manufacturing /Sales. Part of its General Contracts include
+                        Building Construction and Design, Carpentry and Roofing Jobs, and Bookkeeping.
+                    </p>
+                    <hr style="color: brown;">
+                </div>
+
+                <div class="col-sm-4">
+                    <p> Over the years of operations, Eaglon has expanded and has many clients all over the nation. The growth of
+                        the company is accredited to well trained staff and employees and the active management panache of the
+                        Managing Director of the company, Mr. Ogbonna Charles Njoku whose educational, and technical background has
+                        helped in the growth of Eaglon. Mr. Ogbonna holds a certificate in Accounting from Abia State Polytechnic,
+                        Aba, a Diploma in Building Construction Technology from “The Ellis School of Building, Albany House,
+                        Worcester”, and a Bachelor’s Degree in Entrepreneurial & Business Management from National Open University
+                        of Nigeria. He has used his accounting skills to set up the company’s chart of accounts, keep company’s
+                        financial records, process payroll and file yearly tax clearance with the Nigerian Inland Revenue.
+                        Ogbonna’s business management skills has helped the company to manage its building and roofing contracts
+                        and other
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+
+    <section id="services">
+        <div class="container2">
+            <h2>SERVICES</h2>
+            <h3>What We Do</h3>
+            <h3>General Contracts</h3>
+            <div class="row">
+                <div class="col-md-1">
+                    <h4 class="pity">Carpentry & Roofing Works</h4>
+                        <p>Eaglon provides quality carpentry and roofing work. We specialize in all types of roofing and repairs, including
+                         residential and commercial roofing. We coordinate all kinds of roofing and carpentry jobs, and provide all the
+                         necessary materials, labour, and services required to complete any project. Our team of experience experts work
+                         together to ensure projects and contracts are completed just in time. We are focused in making sure our clients’
+                         needs are met and we strive to exceed expectations.
+                        </p>
+
+                </div>
+
+                <div class="col- md-2">
+                    <h4>Bookkeeping</h4>
+                    <p>We also provide bookkeeping services. “Accounting is a language of business,” and many small business owners do not
+                    know how to keep their financial records. We are here to help you set up your company’s Chart of Accounts and set
+                    up your company books manually or using computerized system, complete and prepare your tax clearance for year end filling,
+                    Bank Reconciliations and other financial and accounting reports
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <h4>Block Making & Building Construction</h4>
+                    <p>At Eaglon, we are experts in block making and building constructions. We make most of the blocks we use in building construction.
+                    Our company is client focused, and we tend to use quality blocks in building. We give our clients the best and our buildings
+                    have solid foundations. Our great team delivers every work promptly to customers’ satisfaction. 
+                    </p>
+                    <hr style="color: brown;">
+
+                </div>
+            </div>
+            
+                
+            
+        </div>
+
+    </section>
+
+    <section id="porfolio">
+        <div class="container3">
+            <h2>PORFOLIO</h2>
+            <p>Our staff has increased from 3 to 10, including permanent and casual employees.
+                Eaglon provides consistent training to staff who contribute immensely to the growth of the company. 
+
+             </p>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="images/proj6.jpg" alt="Eaglon">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/proj3.jpg" alt="Eaglon">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/proj2.jpg" alt="Eaglon">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="images/proj2.jpg" alt="Eaglon">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/proj6.jpg" alt="Eaglon">
+                </div>
+                <div class="col-md-4">
+                    <img src="images/proj3.jpg" alt="Eaglon">
+                <br>
+            </div>
+            <br>
+            <hr style="color: brown;">
+            <span></span>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container4">
+            <h2>CONTACT</h2>
+                <h3>Location</h3>
+                    <p>Okechukwu Njoku’s Compound
+                    Umuakali, Nazi,<br> 
+                    Owerri North LGA
+                    Imo State
+                    </p>
+                    <hr>
+                    <p>Email: eaglon1965@gmail.com <br>
+                    Telehone Number: +234 803 737 9124 <br>
+                    +234 806 520 2392                        
+                    </p>
+            
+            
+        </div>
+    </section>
+    
+</body>
+</html>
